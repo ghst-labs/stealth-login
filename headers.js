ba6e95ca94c6cfa9bceee2e3ca0d96047d83f381
Anthony DAmbrosio <anthonytdambrosio@gmail.com>
2022-01-31 19:11:17 -0500

    Fixed

--- headers.js
+++ headers.js
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-const DEFAULT_PORT = 5000;
+const DEFAULT_PORT = 55975;
 
 const scriptUrlPatterns = [
     '*'
@@ -11,7 +11,18 @@ const scriptUrlPatterns = [
 
 
 
-
+// chrome.tabs.query( //get current Tab
+// {
+//     currentWindow: true,
+//     active: true
+// },
+// function(tabArray) {
+//     currentTab = tabArray[0];
+//     chrome.debugger.attach({ //debug at current tab
+//         tabId: currentTab.id
+//     }, version, onAttach.bind(null, currentTab.id));
+// }
+// )
 var tabId = parseInt(window.location.search.substring(1));
 
 function reloadScript() {
@@ -43,197 +54,60 @@ function doStuffWithDom(domContent) {
 }
 
 
+
+
 window.addEventListener("load", function () {
     chrome.debugger.sendCommand({
         tabId: tabId
-    }, "Fetch.enable");
-
-    // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-
+    }, "Network.enable");
+    chrome.debugger.onEvent.addListener(allEventHandler);
 
 
-    // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-    chrome.debugger.onEvent.addListener(onEvent);
-    document.getElementById("cookieNumber").innerText = "0";
-    document.getElementById("port").value = DEFAULT_PORT.toString();
-});
+    function allEventHandler(debuggeeId, message, params) {
 
-window.addEventListener("unload", function () {
-    chrome.debugger.detach({
-        tabId: tabId
-    });
-});
+        if (tabId != debuggeeId.tabId) {
+            return;
+        }
 
-// Creates the request to communicate with the bot on the localhost + port
-
-var requests = {};
-
-const fullfillRequest = (debuggeeId, requestId, reqHeaders) => {
-    chrome.debugger.sendCommand(
-        debuggeeId, "Fetch.continueRequest",
-        {
-            requestId: requestId
-        },
-        function (e) {
-            var headerStore = {};
-            for (var headerKey in reqHeaders) {
-                if (headerKey.startsWith("Cookie"))
-                    headerStore['CookieString'] = reqHeaders[headerKey];
-            }
-            headerStore['Site'] = 'FansEdge';
-            headerStore['UserAgent'] = window.navigator.userAgent;
-            var req = new XMLHttpRequest();
-            var port = document.getElementById("port").value;
-            var baseUrl = "http://localhost:" + port + "/api/receiveHeaders";
-            req.open("POST", baseUrl, true);
-            req.setRequestHeader("Content-type", "application/json");
-            req.send(JSON.stringify(headerStore));
-            req.onreadystatechange = function () {
-                if (this.readyState === XMLHttpRequest.DONE && this.status === 200) {
-                    console.log("Got response 200!");
-                    document.getElementById("cookieNumber").innerText = (Number(document.getElementById("cookieNumber").innerText) + 1).toString();
-                    //first delete cookies
-                    chrome.cookies.getAll({ domain: "fansedge.com" }, function (cookies) {
-                        for (var i = 0; i < cookies.length; i++) {
-                            chrome.cookies.remove({ url: "https://www.fansedge.com" + cookies[i].path, name: cookies[i].name });
-                        }
-                    });
-
-                    //then reload page
-                    chrome.tabs.executeScript(tabId, {
-                        code: '(' + reloadPage + ')();' //argument here is a string but function.toString() returns function's code
-                    }, (results) => {
-                        console.log('page reloaded')
-                    });
-                }
-            }
-
-        });
-}
-
-// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-
-
-
-// NIKE HARVESTING
-function onEvent(debuggeeId, message, params) {
-    if (tabId != debuggeeId.tabId)
-        return;
-    // Gets the port from the extension pop out window
-    var port = document.getElementById("port").value;
-
-    // If the request is a POST request and the URL the request is going to contains the start of the nike login, it will run
-    if (message == "Fetch.requestPaused" && params.request.method == "POST" && params.request.url.startsWith('https://s3.nikecdn.com/login')) {
-
-
-        // At this point, the login request is ready to be sent to nike.com, We must now complete the request --> Get the request id --> use request id to get request body
-        console.log(params)
-        chrome.debugger.sendCommand(
-            {
+        if (message == "Network.responseReceived") { //response return 
+            chrome.debugger.sendCommand({
                 tabId: tabId
-            },
-            "Fetch.fulfillRequest",
-            {
-                requestId: params.requestId,
-                responseCode: 200
-            },
-            () => {
-                const responseBody = chrome.debugger.sendCommand(
-                    {
-                        tabId: tabId
-                    }
-                    ,
-                    "Fetch.getResponseBody",
-                    {
-                        requestId: params.requestId
-                    },
-                    () => {
-                        console.log(responseBody)
-                    }
-                )
-            }
-        )
+            }, "Network.getResponseBody", {
+                "requestId": params.requestId
+            }, function (response) {
+                // you get the response body here!
+                try {
+                    console.log(response.body)
+                    // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+                    // Up until here works
+                    if (response.body.access_token) {
+                        console.log(response)
+
+                        var xhr = new XMLHttpRequest();
+                        var port = document.getElementById("port").value;
+                        var url = "http://localhost:" + port + "/v1/unite";
+                        xhr.open("POST", url, true);
+                        xhr.setRequestHeader('Content-Type', 'application/json');
+                        xhr.send(JSON.stringify({
+                            accessToken: response.access_token
+                        }));
+
+                        // Refresh Page
+                        chrome.tabs.executeScript(tabId, {
+                            code: '(' + reloadPage + ')();' //argument here is a string but function.toString() returns function's code
+                        }, (results) => {
+                            console.log('page reloaded')
+                        });
+                    } // End of if response.body.access_token
+                } catch (error) {
+                    null
+                }
 
 
-        // console.log(response)
+            }); // End of sendCommand
 
-        //handle target cookies
+        } // End of if Netwrok.responseReceived
 
-        // console.log(params)
+    } // End of allEventHandler
 
-        var headerStore = {};
-        headerStore['Site'] = 'Nike';
-        headerStore['Type'] = 'login'
-        headerStore['UserAgent'] = window.navigator.userAgent;
-        var cookieString = "";
-        for (var headerKey in params.request.headers) {
-            if (headerKey.startsWith('X-GyJwza5Z'))
-                headerStore['Header' + headerKey.split('-')[2].toUpperCase()] = params.request.headers[headerKey];
-            else if (headerKey.startsWith("Cookie"))
-                headerStore['CookieString'] = params.request.headers[headerKey];
-        }
-        let deviceInfo = JSON.parse(params.request.postData)
-        headerStore['DeviceInfo'] = JSON.stringify(deviceInfo.device_info)
-        var req = new XMLHttpRequest();
-        var baseUrl = "http://localhost:" + port + "/api/receiveHeaders";
-
-        req.open("POST", baseUrl, true);
-        req.setRequestHeader("Content-type", "application/json");
-
-        req.send(JSON.stringify(headerStore));
-
-
-
-
-
-        req.onreadystatechange = function () {
-            if (this.readyState === XMLHttpRequest.DONE && this.status === 200) {
-                console.log("one login cookie harvested");
-                document.getElementById("cookieNumber").innerText = (Number(document.getElementById("cookieNumber").innerText) + 1).toString();
-
-                //clear all cookies
-                chrome.cookies.getAll({ domain: "target.com" }, function (cookies) {
-                    for (var i = 0; i < cookies.length; i++) {
-                        chrome.cookies.remove({ url: "https://target.com" + cookies[i].path, name: cookies[i].name });
-                    }
-                });
-
-                chrome.cookies.getAll({ domain: "gsp.target.com" }, function (cookies) {
-                    for (var i = 0; i < cookies.length; i++) {
-                        chrome.cookies.remove({ url: "https://gsp.target.com" + cookies[i].path, name: cookies[i].name });
-                    }
-                });
-
-                //clear all erroneous browser data for login.target.com
-                chrome.debugger.sendCommand({
-                    tabId: tabId
-                }, "Storage.clearDataForOrigin", {
-                    origin: 'https://login.target.com',
-                    storageTypes: 'appcache, cookies, file_systems, indexeddb, local_storage, shader_cache, websql, service_workers, cache_storage, all, other'
-                });
-
-                //clear all erroneous browser data for www.target.com
-                chrome.debugger.sendCommand({
-                    tabId: tabId
-                }, "Storage.clearDataForOrigin", {
-                    origin: 'https://www.target.com',
-                    storageTypes: 'appcache, cookies, file_systems, indexeddb, local_storage, shader_cache, websql, service_workers, cache_storage, all, other'
-                });
-
-                //then reload page
-                chrome.tabs.executeScript(tabId, {
-                    code: '(' + reloadPage + ')();' //argument here is a string but function.toString() returns function's code
-                }, (results) => {
-                    console.log('page reloaded')
-                });
-            }
-        }
-        chrome.debugger.sendCommand({
-            tabId: tabId
-        }, "Fetch.failRequest", {
-            requestId: params.requestId,
-            errorReason: 'Failed'
-        });
-    }
-
-}
+}) // End of Window Listener
